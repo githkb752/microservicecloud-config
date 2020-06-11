1dacb0d8b06bdbea2d967ac2f7656ff8f20632c1
githkb752 <61997288+githkb752@users.noreply.github.com>
2020-06-11 22:57:25 +0800

    删除一行

--- microservicecloud-api/src/main/java/com/nskjy/springcloud/entitys/Dept.java
+++ microservicecloud-api/src/main/java/com/nskjy/springcloud/entitys/Dept.java
@@ -11,7 +11,6 @@ import java.io.Serializable;
 @Data
 @Accessors(chain = true)
 public class Dept implements Serializable { // entity Dept orm mysql dept表 // 必须序列化
-    private long deptno; // 部门编号
     private String dname; // 部门名称
     private String db_source; // 来自哪个数据库，微服务架构每个服务可以对应一个数据库，同一个信息可以存储到不同数据库
 
